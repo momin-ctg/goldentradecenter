b29cee28274769c097d08053f1a2b002f15fe5ec
momin-ctg <mominriyadh@gmail.com>
2016-10-29 23:18:54 +0600

    update index.php,header.php, footer.php

---
+++ footer.php
@@ -0,0 +1,120 @@
+<!-- BEGIN FOOTER -->
+<footer id="footer" class="footer">
+
+	<div class="footer-holder">
+		<div class="footer-frame">
+			<!-- Footer Widgets -->
+			<div class="footer-widgets">
+				<div class="container clearfix">
+
+					<div class="grid_5">
+						<!-- Text Widget -->
+						<div class="text-widget widget widget__footer">
+							<h3 class="widget-title">Company Overview</h3>
+							<div class="widget-content">
+								<h5>Golden Trade Center</h5>
+								<p>is considered as one of the leading exporter of tobacco products in Bangladesh. Since
+									2007, Golden Trade Center has been operating export and import business in Middle
+									East. The company also has operations in the Asian countries as well. Based on the
+									decision of the owner, to diversify business we started to supply other products in
+									local markets and export in foreign market.<br>
+									<strong> Trade license no </strong> 85355 <br>
+									<strong>Export registration certificate no</strong> RA 73893 <br>
+									<strong>Member Chittagong Chamber of Commerce, Bangladesh</strong>
+								</p>
+							</div>
+						</div>
+						<!-- /Text Widget -->
+					</div>
+					<div class="grid_3">
+						<!-- Twitter Widget -->
+						<div class="twitter-widget widget widget__footer">
+							<h3 class="widget-title">Twitter</h3>
+							<div class="widget-content">
+								<!-- <ul id="twitter-feed" class="twitter_update_list"></ul> -->
+							</div>
+						</div>
+						<!-- /Twitter Widget -->
+					</div>
+					<div class="grid_4">
+						<!-- Flickr Widget -->
+						<div class="flickr-widget widget widget__footer">
+							<h3 class="widget-title">Flicker Feed</h3>
+							<div class="widget-content">
+								<!-- Flickr images will appear here -->
+								<!-- <ul id="flickr" class="thumbs"></ul> -->
+							</div>
+						</div>
+						<!-- /Flickr Widget -->
+					</div>
+
+				</div>
+			</div>
+			<!-- /Footer Widgets -->
+
+			<!-- Copyright -->
+			<div class="copyright">
+				<div class="container clearfix">
+					<div class="grid_12">
+						<div class="clearfix">
+							<div class="copyright-primary">
+								&copy; 2007 Golden Trade Center<span class="separator">|</span> Export Import
+								Businesses.
+							</div>
+							<div class="copyright-secondary">
+								Design & Devloped By <a href="http://www.opensourcit.net"><img
+										src="images/opensourceit_developer_designer.png" alt="Opensource-It" width="100"
+										height="auto"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /Copyright -->
+		</div>
+	</div>
+
+</footer>
+<!-- END FOOTER -->
+
+</div>
+<!-- END WRAPPER -->
+
+
+<!-- Javascript Files
+================================================== -->
+
+<!-- initialize jQuery Library -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')</script>
+<script src="js/jquery-migrate-1.1.1.min.js"></script>
+<!-- Modernizr -->
+<script src="js/modernizr.custom.17475.js"></script>
+<!-- easing plugin -->
+<script src="js/jquery.easing.min.js"></script>
+<!-- Prettyphoto -->
+<script src="js/jquery.prettyPhoto.js"></script>
+<!-- Superfish -->
+<script src="js/jquery.mobilemenu.js"></script>
+<!-- superfish -->
+<script src="js/jquery.superfish-1.5.0.js"></script>
+<!-- Twitter -->
+<script src="js/jquery.twitter.js"></script>
+<!-- Flickr -->
+<script src="js/jflickrfeed.js"></script>
+<!-- ElastiSlide Carousel -->
+<script src="js/jquery.elastislide.js"></script>
+<!-- jQuery REVOLUTION Slider  -->
+<script src="js/jquery.themepunch.plugins.min.js"></script>
+<script src="js/jquery.themepunch.revolution.min.js"></script>
+<!-- Isotope -->
+<script src="js/jquery.isotope.min.js"></script>
+<script src="js/jquery.imagesloaded.min.js"></script>
+<!-- Flexslider -->
+<script src="js/jquery.flexslider.js"></script>
+
+<!-- Custom -->
+<script src="js/custom.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie7 no-js" lang="en">     <![endif]-->
+<!--[if lte IE 8]>
+<html class="ie8 no-js" lang="en">     <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="not-ie no-js" lang="en">  <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+	================================================== -->
+	<meta charset="utf-8">
+	<title>Golden Trade Center | Export & Import Businesses</title>
+	<meta name="description"
+	      content="Golden Trade Centre is considered as one of the leading exporter of tobacco products in Bangladesh. Since 2007, Golden Trade Centre has been operating export and import business in Middle East. The company also has operations in the Asian countries as well.">
+	<meta name="author" content="Mohd. Shah Alam">
+	<meta name="keywords"
+	      content="export, import, chittagong, bangladesh, jute, betel leaf, tobacco, handicraft, hotel, resorts, suppliers">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
+	<!-- Mobile Specific Metas
+	================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+	<!-- CSS
+	================================================== -->
+	<!-- Normalize default styles -->
+	<link rel="stylesheet" href="css/normalize.css" media="screen"/>
+	<!-- Skeleton grid system -->
+	<link rel="stylesheet" href="css/skeleton.css" media="screen"/>
+	<!-- Base Template Styles-->
+	<link rel="stylesheet" href="css/base.css" media="screen"/>
+	<!-- Template Styles-->
+	<link rel="stylesheet" href="css/style.css" media="screen"/>
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css" media="screen"/>
+	<!-- Prettyphoto -->
+	<link rel="stylesheet" href="css/prettyPhoto.css" media="screen"/>
+	<!-- FontAwesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css" media="screen"/>
+	<!-- REVOLUTION BANNER CSS SETTINGS -->
+	<link rel="stylesheet" href="css/settings.css" media="screen"/>
+	<!-- Flexslider -->
+	<link rel="stylesheet" href="css/flexslider.css" media="screen"/>
+	<!-- Layout and Media queries-->
+	<link rel="stylesheet" href="css/media-queries.css" media="screen"/>
+
+	<!--[if lt IE 9]>
+	<link rel="stylesheet" href="css/ie/ie8.css" media="screen"/>
+	<![endif]-->
+
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-144x144.png">
+
+
+</head>
+<body>
+
+<!-- BEGIN WRAPPER -->
+<div id="wrapper">
+
+	<div class="wrapp-holder">
+		<div class="wrap-frame">
+			<!-- BEGIN HEADER -->
+			<header id="header" class="header">
+
+				<!-- Top Header -->
+				<div class="header-top">
+					<div class="container clearfix">
+						<div class="grid_12">
+							<!-- Top Menu -->
+							<ul class="header-top-menu unstyled">
+								<li><a href="#">goldentradecentre@yahoo.com</a></li>
+								<li><a href="#">+880 01818529291</a></li>
+								<li><a href="http://www.goldentradecenter.com/webmail">Web Mail</a></li>
+
+							</ul>
+							<!-- /Top Menu -->
+
+							<!-- Social Links -->
+							<ul class="social-links unstyled">
+								<li class="ico-twitter"><a href="#">Twitter</a></li>
+								<li class="ico-facebook"><a href="#">Facebook</a></li>
+								<li class="ico-googleplus"><a href="#">Google+</a></li>
+							</ul>
+							<!-- /Social Links -->
+						</div>
+					</div>
+				</div>
+				<!-- /Top Header -->
+
+				<!-- Main Header -->
+				<div class="header-main">
+					<div class="container clearfix">
+						<div class="grid_12 hr-bottom">
+
+							<!-- BEGIN LOGO -->
+							<div id="logo">
+								<!-- Image based Logo
+								<a href="index.html"><img src="images/logo.png" alt="Opensource-It" width="200" height="80" /></a> -->
+
+								<!-- Text based Logo -->
+								<h1><a href="index.html">Golden Trade Center</a></h1>
+								<p class="tagline">Exporter, Importer & Suppliers</p>
+
+							</div>
+							<!-- END LOGO -->
+
+
+							<!-- BEGIN NAVIGATION -->
+							<nav class="primary">
+								<ul class="sf-menu">
+									<li class="current-menu-item">
+										<a href="index.html">About Us <span><i>|</i> Why Choose Us</span></a>
+										<!-- Sub Navigation -->
+										<!-- <ul>
+											<li class="current-menu-item"><a href="about.html">About Us</a></li>
+											<li><a href="columns.html">Columns</a></li>
+
+											<li><a href="pricing-tables.html">Pricing Tables</a></li>
+											<li><a href="elements.html">Page Elements</a></li>
+										</ul> -->
+										<!-- /Sub Navigation -->
+									</li>
+									<li><a href="services.html">Services<span><i>|</i> Our Services</span></a></li>
+									<li>
+										<a href="products.html">Products <span><i>|</i> Our Products</span></a>
+										<!-- Sub Navigation -->
+										<!-- 	<ul>
+												<li><a href="portfolio-2cols.html">Portfolio 2 cols</a></li>
+												<li><a href="portfolio-3cols.html">Portfolio 3 cols</a></li>
+												<li><a href="portfolio-4cols.html">Portfolio 4 cols</a></li>
+												<li><a href="portfolio-details.html">Single Project</a></li>
+											</ul> -->
+										<!-- /Sub Navigation -->
+									</li>
+
+									<li>
+										<a href="contacts.html">Contacts <span><i>|</i> Get in Touch</span></a>
+									</li>
+								</ul>
+							</nav>
+							<!-- END NAVIGATION -->
+
+						</div>
+					</div>
+				</div>
+				<!-- /Main Header -->
+
+			</header>
+			<!-- END HEADER -->
