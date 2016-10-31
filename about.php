c3b73f8d1ef1d991fac21d906200a08e86e6b041
momin-ctg <mominriyadh@gmail.com>
2016-10-31 09:01:22 +0600

    update about page

---
+++ about.php
@@ -0,0 +1,213 @@
+<?php include( 'header.php' ); ?>
+
+
+	<!-- BEGIN PAGE TITLE -->
+	<div id="page-title" class="page-title">
+		<div class="container clearfix">
+			<div class="grid_12">
+				<div class="page-title-holder clearfix">
+					<h1>About Us</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END PAGE TITLE -->
+
+
+	<!-- BEGIN CONTENT WRAPPER -->
+	<div id="content-wrapper" class="content-wrapper">
+		<div class="container">
+
+			<div class="clearfix">
+				<div class="grid_4">
+					<h2>Our Mission</h2>
+					<span class="dropcap dropcap-rounded dropcap-large dropcap-style2">1</span>
+					<h6>Our mission is to build</h6>
+					<p> long term relationships with our customers and clients and provide best quality supplies </p>
+
+					<div class="spacer spacer__small"></div>
+
+					<span class="dropcap dropcap-rounded dropcap-large dropcap-style2">2</span>
+					<h6>Our vision is to provide</h6>
+					<p> quality services that exceeds the expectations of our esteemed customers and clients</p>
+
+					<div class="spacer spacer__small"></div>
+
+					<span class="dropcap dropcap-rounded dropcap-large dropcap-style2">3</span>
+					<h6>Regional expansion in the field</h6>
+					<p> of export and import and develop a strong relationship with customers and to build good
+						reputation in the field of export and import business</p>
+				</div>
+				<div class="grid_4">
+					<h2>Purpose</h2>
+					<h5>To be a leading exporter and importer by providing best products, enhanced services and
+						establishing long term relationship</h5>
+					<p><span class="dropcap dropcap-style1">G</span>olden Trade Center is considered as one of the
+						leading exporter of tobacco products in Bangladesh. Since 2007, Golden Trade Centre has been
+						operating export and import business in Middle East. The company also has operations in the
+						Asian countries as well. Based on the decision of the owner, to diversify business we started to
+						supply other products in local markets and export in foreign market. We are exporter, importer
+						and supplier of all kinds of foodstuffs, jute products, pottery, readymade garments,
+						handicrafts, glassware, ceramics and other items.</p>
+				</div>
+				<div class="grid_4">
+					<h2>Our Product</h2>
+					<!-- Progress Bar -->
+					<div class="progress-bar">
+						<h4>Tobacco </h4>
+						<div class="progress-bar-holder">
+							<div class="progress-bar-value value-80"></div>
+						</div>
+					</div>
+					<!-- /Progress Bar -->
+
+					<!-- Progress Bar -->
+					<div class="progress-bar">
+						<h4>Betel Leaf</h4>
+						<div class="progress-bar-holder">
+							<div class="progress-bar-value value-40"></div>
+						</div>
+					</div>
+					<!-- /Progress Bar -->
+
+					<!-- Progress Bar -->
+					<div class="progress-bar">
+						<h4>Jute</h4>
+						<div class="progress-bar-holder">
+							<div class="progress-bar-value value-90"></div>
+						</div>
+					</div>
+					<!-- /Progress Bar -->
+
+					<!-- Progress Bar -->
+					<div class="progress-bar">
+						<h4>Garments</h4>
+						<div class="progress-bar-holder">
+							<div class="progress-bar-value value-60"></div>
+						</div>
+					</div>
+					<!-- /Progress Bar -->
+
+					<!-- Progress Bar -->
+					<div class="progress-bar last">
+						<h4>Handicraft</h4>
+						<div class="progress-bar-holder">
+							<div class="progress-bar-value value-70"></div>
+						</div>
+					</div>
+					<!-- /Progress Bar -->
+
+
+				</div>
+			</div>
+
+			<div class="hr hr-dashed"></div>
+
+			<div class="clearfix">
+				<div class="grid_12">
+					<h2>Our First Line Products</h2>
+				</div>
+			</div>
+
+			<div class="team-holder clearfix">
+				<div class="item-team grid_3">
+					<figure class="team-img">
+						<img src="images/samples/gtc_jute.jpg" height="220" width="220" alt="">
+					</figure>
+					<hgroup>
+						<h4>Jute</h4>
+						<h5>A strong, coarse fiber used for making burlap, gunny, cordage, etc.</h5>
+					</hgroup>
+					<div class="team-excerpt">Golden Trade Center is your best comprehensive partner for your desired
+						products. We export jute items, mustard oil, ready-made garments, puffed rice, tobacco and many
+						more. We export products to Middle East, India and Pakistan.
+					</div>
+					<footer class="team-footer">
+						<!-- Social Links -->
+						<!-- 	<ul class="social-links unstyled">
+								<li class="ico-twitter"><a href="#">Twitter</a></li>
+								<li class="ico-facebook"><a href="#">Facebook</a></li>
+								<li class="ico-googleplus"><a href="#">Google+</a></li>
+								<li class="ico-dribbble"><a href="#">Dribbble</a></li>
+							</ul> -->
+						<!-- /Social Links -->
+					</footer>
+				</div>
+				<div class="item-team grid_3">
+					<figure class="team-img">
+						<img src="images/samples/gtc_tobacco.jpg" height="220" width="220" alt="">
+					</figure>
+					<hgroup>
+						<h4>Tobacco</h4>
+						<h5>Lorem ipsum dolor sit amet.</h5>
+					</hgroup>
+					<div class="team-excerpt">Golden Trade Center is your best comprehensive partner for your desired
+						products. We export jute items, mustard oil, ready-made garments, puffed rice, tobacco and many
+						more. We export products to Middle East, India and Pakistan.
+					</div>
+					<footer class="team-footer">
+						<!-- Social Links -->
+						<!-- <ul class="social-links unstyled">
+							<li class="ico-twitter"><a href="#">Twitter</a></li>
+							<li class="ico-facebook"><a href="#">Facebook</a></li>
+							<li class="ico-googleplus"><a href="#">Google+</a></li>
+						</ul> -->
+						<!-- /Social Links -->
+					</footer>
+				</div>
+				<div class="item-team grid_3">
+					<figure class="team-img">
+						<img src="images/samples/gtc_jordda.jpg" height="220" width="220" alt="">
+					</figure>
+					<hgroup>
+						<h4>Zarda</h4>
+						<h5>Hakimpuri Zarda is best zarda. It is vastly used with betel leaf. It is famous for its
+							fragrance, unique taste and quality. It is vastly used with betel leaf.</h5>
+					</hgroup>
+					<div class="team-excerpt">Golden Trade Center is your best comprehensive partner for your desired
+						products. We export jute items, mustard oil, ready-made garments, puffed rice, tobacco and many
+						more. We export products to Middle East, India and Pakistan.
+					</div>
+					<footer class="team-footer">
+						<!-- Social Links -->
+						<!-- <ul class="social-links unstyled">
+							<li class="ico-twitter"><a href="#">Twitter</a></li>
+							<li class="ico-facebook"><a href="#">Facebook</a></li>
+							<li class="ico-googleplus"><a href="#">Google+</a></li>
+							<li class="ico-dribbble"><a href="#">Dribbble</a></li>
+						</ul> -->
+						<!-- /Social Links -->
+					</footer>
+				</div>
+				<div class="item-team grid_3">
+					<figure class="team-img">
+						<img src="images/samples/gtc_handicraft.jpg" height="220" width="220" alt="">
+					</figure>
+					<hgroup>
+						<h4>Handicraft</h4>
+						<h5>Lorem ipsum dolor sit amet.</h5>
+					</hgroup>
+					<div class="team-excerpt">Golden Trade Center is your best comprehensive partner for your desired
+						products. We export jute items, mustard oil, ready-made garments, puffed rice, tobacco and many
+						more. We export products to Middle East, India and Pakistan.
+					</div>
+					<footer class="team-footer">
+						<!-- Social Links -->
+						<!-- <ul class="social-links unstyled">
+							<li class="ico-twitter"><a href="#">Twitter</a></li>
+							<li class="ico-facebook"><a href="#">Facebook</a></li>
+							<li class="ico-googleplus"><a href="#">Google+</a></li>
+							<li class="ico-dribbble"><a href="#">Dribbble</a></li>
+						</ul> -->
+						<!-- /Social Links -->
+					</footer>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<!-- END CONTENT WRAPPER -->
+	</div>
+	</div>
+
+<?php include( 'footer.php' ); ?>
